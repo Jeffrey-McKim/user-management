34b4dec0b9d541cc9e415df8be6fdd4af9c2a1b5
Jeffrey-McKim <38574057+Jeffrey-McKim@users.noreply.github.com>
2018-07-26 15:28:04 -0700

    Add files via upload

--- demo.sql
+++ demo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 26, 2018 at 11:44 PM
+-- Generation Time: Jul 27, 2018 at 12:27 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -25,29 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `expensedata`
---
-
-CREATE TABLE `expensedata` (
-  `amount` decimal(10,0) NOT NULL,
-  `date` date NOT NULL,
-  `itemtype` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `expensedata`
---
-
-INSERT INTO `expensedata` (`amount`, `date`, `itemtype`, `name`) VALUES
-('0', '0000-00-00', '', ''),
-('0', '1333-11-23', 'huge', ''),
-('0', '1995-01-23', 'it was big', 'thekochbrothers19@gmail.com'),
-('0', '1995-01-23', 'its free real estate', 'tom');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `usermanagement`
 --
 
@@ -60,18 +37,15 @@ CREATE TABLE `usermanagement` (
   `password` varchar(255) NOT NULL,
   `verification_key` varchar(255) NOT NULL,
   `active` tinyint(1) NOT NULL,
-  `forgot_status` tinyint(1) NOT NULL,
-  `amount` decimal(11,0) NOT NULL,
-  `date` date NOT NULL,
-  `itemtype` varchar(255) NOT NULL
+  `forgot_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `usermanagement`
 --
 
-INSERT INTO `usermanagement` (`id`, `username`, `firstname`, `lastname`, `email`, `password`, `verification_key`, `active`, `forgot_status`, `amount`, `date`, `itemtype`) VALUES
-(119, 'tom', '', '', 'jmckim@bluejaybuilds.com', '166ee015c0e0934a8781e0c86a197c6e', '34b7da764b21d298ef307d04d8152dc5', 1, 0, '0', '0000-00-00', '');
+INSERT INTO `usermanagement` (`id`, `username`, `firstname`, `lastname`, `email`, `password`, `verification_key`, `active`, `forgot_status`) VALUES
+(121, 'tom', '', '', 'jmckim@bluejaybuilds.com', '34b7da764b21d298ef307d04d8152dc5', '34b7da764b21d298ef307d04d8152dc5', 1, 0);
 
 --
 -- Indexes for dumped tables
@@ -93,7 +67,7 @@ ALTER TABLE `usermanagement`
 -- AUTO_INCREMENT for table `usermanagement`
 --
 ALTER TABLE `usermanagement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
